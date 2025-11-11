429da15ddb4b26c6571491ba67a3f1dc29b4c61f
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2025-11-11 10:57:15 +0800

    fix: Change server port from 8080 to 8000 for local development

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import app from "../src/app";
 
-const port = 8080;
+const port = 8000;
 
 if (process.env.NODE_ENV !== "production") {
   app.listen(port, () => console.log(`Server is running on port ${port}`));
